9e79459e06c69bba9c9c1f7c72bb6420dbe02d72
thomas <bezille.thomasb@gmail.com>
2024-11-14 17:42:32 +0100

    📦 NEW: Sorts in alphabetical order and last cleaning + test

--- organiser_des_donnees.py
+++ organiser_des_donnees.py
@@ -12,5 +12,8 @@ with open(ORIGIN_FILE, "r", encoding="utf-8") as file:
     sanitized_names_list = sanitized_names.split(" ")
     
 with open(DESTINATION_FILE, "w", encoding="utf-8") as file:
+    sanitized_names_list.sort()
     for name in sanitized_names_list:
+        if name == "":
+            continue
         file.write(name + "\n")
